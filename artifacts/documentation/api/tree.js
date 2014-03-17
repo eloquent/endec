8e9f094a9ffb47ef7ea3d57b04b914c248ef593d
Woodhouse <contact@icecave.com.au>
2014-03-17 03:43:42 +0000

    Publishing artifacts from build 5.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -1,11 +1,12 @@
 var tree = [["Eloquent",,"",
                 [["Endec",,"",
                 [["Encoding","Eloquent\/Endec\/Encoding.html","",
-                [["AbstractCodec","Eloquent\/Endec\/Encoding\/AbstractCodec.html","",
-                [        ]],["Base64Decoder","Eloquent\/Endec\/Encoding\/Base64Decoder.html"," < AbstractCodec",
-                [        ]],["Base64Encoder","Eloquent\/Endec\/Encoding\/Base64Encoder.html"," < AbstractCodec",
-                [        ]],["CodecInterface","Eloquent\/Endec\/Encoding\/CodecInterface.html","",
-                [        ]],["HexDecoder","Eloquent\/Endec\/Encoding\/HexDecoder.html"," < AbstractCodec",
-                [        ]],["HexEncoder","Eloquent\/Endec\/Encoding\/HexEncoder.html"," < AbstractCodec",
+                [["Base64Decoder","Eloquent\/Endec\/Encoding\/Base64Decoder.html"," < AbstractDataTransform",
+                [        ]],["Base64Encoder","Eloquent\/Endec\/Encoding\/Base64Encoder.html"," < AbstractDataTransform",
+                [        ]],["HexDecoder","Eloquent\/Endec\/Encoding\/HexDecoder.html"," < AbstractDataTransform",
+                [        ]],["HexEncoder","Eloquent\/Endec\/Encoding\/HexEncoder.html"," < AbstractDataTransform",
+                [        ]]        ]],["Transform","Eloquent\/Endec\/Transform.html","",
+                [["AbstractDataTransform","Eloquent\/Endec\/Transform\/AbstractDataTransform.html","",
+                [        ]],["DataTransformInterface","Eloquent\/Endec\/Transform\/DataTransformInterface.html","",
                 [        ]]        ]]        ]]        ]]        ]
 
